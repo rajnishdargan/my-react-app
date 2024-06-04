d196caef53c921bec2314dee2105c2fd7fb6680f
Rajnish Dargan <rajnish.code30@gmail.com>
2024-06-04 10:11:55 +0530

    Editor integration - fixed duplicate loading - Css issue to be addressed

--- src/components/QuestionSetEditor.js
+++ src/components/QuestionSetEditor.js
@@ -1,21 +1,27 @@
-// src/components/QuestionSetEditor.js
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { questionSetEditorConfig } from '../data';
 
 const QuestionSetEditor = () => {
   const editorRef = useRef(null);
+  // let [isAppended, setIsAppended] = useState(false);
+  let isAppended = false;
 
   useEffect(() => {
-    const editorConfig = questionSetEditorConfig;
-    const questionsetEditorElement = document.createElement('lib-questionset-editor');
-    questionsetEditorElement.setAttribute('editor-config', JSON.stringify(editorConfig));
+    if (!isAppended) {
+      const editorConfig = questionSetEditorConfig;
+      const questionsetEditorElement = document.createElement('lib-questionset-editor');
+      questionsetEditorElement.setAttribute('editor-config', JSON.stringify(editorConfig));
 
-    questionsetEditorElement.addEventListener('editorEmitter', (event) => {
-      console.log('On editorEvent', event);
-    });
+      questionsetEditorElement.addEventListener('editorEmitter', (event) => {
+        console.log('On editorEvent', event);
+      });
 
-    if (editorRef.current) {
-      editorRef.current.appendChild(questionsetEditorElement);
+      if (editorRef.current) {
+        console.log('Element appended');
+        editorRef.current.appendChild(questionsetEditorElement);
+        // setIsAppended(true);
+        isAppended = true;
+      }
     }
   }, []);
 
